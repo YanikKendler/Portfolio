db2c8664c2daaa9784fc8111f3ad15f438b63e1b
elYanuki <pc.mail.yanik@gmail.com>
2023-01-14 21:35:56 +0100

    new galery system finished

--- updateImages.bat
+++ updateImages.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
-set "folder=./img/photoshop"
+set "src=./img/photoshop"
+set "alt=./img/photoshop/alt"
 set "output=./js/imagePaths.js"
 
 rem Clear the output file
@@ -8,8 +9,16 @@ echo.>"%output%"
 
 rem Get the list of files
 echo let imgSrcs = [ >"%output%"
-for /f "delims=" %%f in ('dir /b /a-d "%folder%"') do (
-    echo "./img/photoshop/%%f", >>"%output%"
+for /f "delims=" %%f in ('dir /b /a-d "%src%"') do (
+echo "%%f", >>"%output%"
+)
+echo ] >>"%output%"
+
+rem Get the list of files from alt folder
+echo. >>%output%
+echo let altSrcs = [ >>"%output%"
+for /f "delims=" %%f in ('dir /b /a-d "%alt%"') do (
+echo "%%f", >>"%output%"
 )
 echo ] >>"%output%"
 
